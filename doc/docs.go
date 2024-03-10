b7337ea15f02a0f054a3396d4c05133725607259
Hearts Fiddlesticks <heartsfi@un-m6.kzn.21-school.ru>
2024-03-10 19:23:09 +0300

    start making clean arch

---
+++ doc/docs.go
@@ -0,0 +1,47 @@
+package docs
+
+const doc = `
+		<!doctype html>
+		<html>
+		<head>
+			<meta charset="utf-8">
+			<title>Places</title>
+		</head>
+		<body>
+		<h5>Total</h5>
+		<ul>
+			{{range .Places}}
+				<li>
+					<div>{{.Name}}</div>
+					<div>{{.Address}}</div>
+					<div>{{.Phone}}</div>
+				</li>
+			{{end}}
+		</ul>
+		<a href="/places/show?page={{.PrevPage}}">Previous</a>
+		<a href="/places/show?page={{.NextPage}}">Next</a>
+		</body>
+		</html>
+	`
+
+const docSearchResult = `
+		<!doctype html>
+		<html>
+		<head>
+			<meta charset="utf-8">
+			<title>Search result :</title>
+		</head>
+		<body>
+		<h5>Total : {{.Total}}</h5>
+		<ul>
+			{{range .RspRes.Hits.Hits}}
+				<li>
+					<div>{{.Source.Name}}</div>
+					<div>{{.Source.Address}}</div>
+					<div>{{.Source.Phone}}</div><br />
+				</li>
+			{{end}}
+		</ul>
+		</body>
+		</html>
+	`
